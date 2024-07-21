c7b733e091d54c04c1481455e6fd83570c0178aa
Lucas Marques Mendes Lucas <lucasmqmendes@gmail.com>
2024-07-21 00:12:51 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/algtech/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.algtech.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.algtech.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+}
